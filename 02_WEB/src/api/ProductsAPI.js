62a290a081f9dad0fde589b590d1671279665a13
Huowsng <huongvlqt007@gmail.com>
2024-05-14 18:56:21 +0700

    14/5- 7.00

--- 02_WEB/src/api/ProductsAPI.js
+++ 02_WEB/src/api/ProductsAPI.js
@@ -1,38 +1,53 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import API_URL from './baseAPI';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import API_URL from "./baseAPI";
 
 function ProductsAPI() {
-    const [products, setProducts] = useState([]);
-    const [callback, setCallback] = useState(false);
-    const [category, setCategory] = useState('');
-    const [sort, setSort] = useState('');
-    const [search, setSearch] = useState('');
-    const [page, setPage] = useState(1);
-    const [result, setResult] = useState(0);
-    const test = 'aaaaaa';
+  const [products, setProducts] = useState([]);
+  const [callback, setCallback] = useState(false);
+  const [category, setCategory] = useState("");
+  const [sort, setSort] = useState("");
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+  const [result, setResult] = useState(0);
+  const test = "aaaaaa";
 
-    useEffect(() => {
-        const getProducts = async () => {
-            const res = await axios.get(`${API_URL}/api/products?limit=${page * 9}&${category}&${sort}&title[regex]=${search}`);
-            // console.log('price' + JSON.stringify(res.data.products[0].types));
-            // console.log(res.data.products.types.length)
-            setProducts(res.data.products);
-            setResult(res.data.result);
-        };
-        getProducts();
-    }, [callback, category, sort, search, page]);
+  const getProductsWithRole1 = async () => {
+    const res = await axios.get(
+      `${API_URL}/api/products?limit=${
+        page * 9
+      }&${category}&${sort}&title[regex]=${search}&role=1`
+    );
+    setProducts(res.data.products);
+    setResult(res.data.result);
+  };
 
-    return {
-        products: [products, setProducts],
-        callback: [callback, setCallback],
-        category: [category, setCategory],
-        sort: [sort, setSort],
-        search: [search, setSearch],
-        page: [page, setPage],
-        result: [result, setResult],
-        test,
-    };
+  const getProductsWithRole0 = async () => {
+    const res = await axios.get(
+      `${API_URL}/api/products?limit=${
+        page * 9
+      }&${category}&${sort}&title[regex]=${search}&role=0`
+    );
+    setProducts(res.data.products);
+    setResult(res.data.result);
+  };
+
+  useEffect(() => {
+    getProductsWithRole1(); // You can call either function here
+  }, [callback, category, sort, search, page]);
+
+  return {
+    products: [products, setProducts],
+    callback: [callback, setCallback],
+    category: [category, setCategory],
+    sort: [sort, setSort],
+    search: [search, setSearch],
+    page: [page, setPage],
+    result: [result, setResult],
+    getProductsWithRole1, // Expose the functions to use them outside
+    getProductsWithRole0, // Expose the functions to use them outside
+    test,
+  };
 }
 
 export default ProductsAPI;
--- 02_WEB/src/components/mainpages/products/Products.js
+++ 02_WEB/src/components/mainpages/products/Products.js
@@ -1,108 +1,108 @@
-import React, { useContext, useState } from 'react';
-import { GlobalState } from '../../../GlobalState';
-import ProductItem from '../utils/productItem/ProductItem';
-import Loading from '../utils/loading/Loading';
-import axios from 'axios';
-import Filters from './Filters';
-import LoadMore from './LoadMore';
-import { Link } from 'react-router-dom';
-import API_URL from '../../../api/baseAPI';
+import React, { useContext, useState } from "react";
+import { GlobalState } from "../../../GlobalState";
+import ProductItem from "../utils/productItem/ProductItem";
+import Loading from "../utils/loading/Loading";
+import axios from "axios";
+import Filters from "./Filters";
+import LoadMore from "./LoadMore";
+import { Link } from "react-router-dom";
+import API_URL from "../../../api/baseAPI";
 
 function Products() {
-    const state = useContext(GlobalState);
-    const [products, setProducts] = state.productsAPI.products??[];
-    console.log(products);
-    const [isAdmin] = state.userAPI.isAdmin;
-    const [token] = state.token;
-    const [callback, setCallback] = state.productsAPI.callback;
-    const [loading, setLoading] = useState(false);
-    const [isCheck, setIsCheck] = useState(false);
-    const handleCheck = (id) => {
-        products.forEach((product) => {
-            if (product._id === id) product.checked = !product.checked;
-        });
-        setProducts([...products]);
-    };
+  const state = useContext(GlobalState);
+  const [products, setProducts] = state.productsAPI.products ?? [];
+  console.log(products);
+  const [isAdmin] = state.userAPI.isAdmin;
+  const [token] = state.token;
+  const [callback, setCallback] = state.productsAPI.callback;
+  const [loading, setLoading] = useState(false);
+  const [isCheck, setIsCheck] = useState(false);
+  const handleCheck = (id) => {
+    products.forEach((product) => {
+      if (product._id === id) product.checked = !product.checked;
+    });
+    setProducts([...products]);
+  };
 
-    const deleteProduct = async (id, public_id) => {
-        try {
-            setLoading(true);
-            const destroyImg = axios.post(
-                `${API_URL}/api/destroy`,
-                { public_id },
-                {
-                    headers: { Authorization: token },
-                },
-            );
-            const deleteProduct = axios.delete(`${API_URL}/api/products/${id}`, {
-                headers: { Authorization: token },
-            });
-            await destroyImg;
-            await deleteProduct;
-            setCallback(!callback);
-            alert('Product deleted successfully2');
-        } catch (err) {
-            // alert(err.response.data.msg);
-            console.log(err);
+  const deleteProduct = async (id, public_id) => {
+    try {
+      setLoading(true);
+      const destroyImg = axios.post(
+        `${API_URL}/api/destroy`,
+        { public_id },
+        {
+          headers: { Authorization: token },
         }
-    };
+      );
+      const deleteProduct = axios.delete(`${API_URL}/api/products/${id}`, {
+        headers: { Authorization: token },
+      });
+      await destroyImg;
+      await deleteProduct;
+      setCallback(!callback);
+      alert("Product deleted successfully2");
+    } catch (err) {
+      // alert(err.response.data.msg);
+      console.log(err);
+    }
+  };
 
-    const checkAll = () => {
-        products?.forEach((product) => {
-            product.checked = !isCheck;
-        });
-        setProducts([...products]);
-        setIsCheck(!isCheck);
-    };
+  const checkAll = () => {
+    products?.forEach((product) => {
+      product.checked = !isCheck;
+    });
+    setProducts([...products]);
+    setIsCheck(!isCheck);
+  };
 
-    const deleteAll = () => {
-        products.forEach((product) => {
-            if (product.checked) deleteProduct(product._id, product.images.public_id);
-        });
-    };
+  const deleteAll = () => {
+    products.forEach((product) => {
+      if (product.checked) deleteProduct(product._id, product.images.public_id);
+    });
+  };
 
-    if (loading)
-        return (
-            <div>
-                <Loading />
-            </div>
-        );
+  if (loading)
     return (
-        <>
-            <Filters />
-            <div className="header-information">
-                <p className="header-label">Cửa hàng</p>
-                <div className="header-direction">
-                    <Link to="/">Trang chủ /</Link>
-                    <Link to="/products">Cửa hàng</Link>
-                </div>
-            </div>
-
-            {isAdmin && (
-                <div className="delete-all">
-                    <span>Chọn Tất cả </span>
-                    <input type="checkbox" checked={isCheck} onChange={checkAll} />
-                    <button onClick={deleteAll}>Xoá hết</button>
-                </div>
-            )}
-
-            <div className="products">
-                {products?.map((product) => {
-                    return (
-                        <ProductItem
-                            key={product._id}
-                            product={product}
-                            isAdmin={isAdmin}
-                            deleteProduct={deleteProduct}
-                            handleCheck={handleCheck}
-                        />
-                    );
-                })}
-            </div>
-            <LoadMore />
-            {products?.length === 0 && <Loading />}
-        </>
+      <div>
+        <Loading />
+      </div>
     );
+  return (
+    <>
+      <Filters />
+      <div className="header-information">
+        <p className="header-label">Cửa hàng</p>
+        <div className="header-direction">
+          <Link to="/">Trang chủ /</Link>
+          <Link to="/products">Cửa hàng</Link>
+        </div>
+      </div>
+
+      {isAdmin && (
+        <div className="delete-all">
+          <span>Chọn Tất cả </span>
+          <input type="checkbox" checked={isCheck} onChange={checkAll} />
+          <button onClick={deleteAll}>Xoá hết</button>
+        </div>
+      )}
+
+      <div className="products">
+        {products?.map((product) => {
+          return (
+            <ProductItem
+              key={product._id}
+              product={product}
+              isAdmin={isAdmin}
+              deleteProduct={deleteProduct}
+              handleCheck={handleCheck}
+            />
+          );
+        })}
+      </div>
+      <LoadMore />
+      {products?.length === 0 && <Loading />}
+    </>
+  );
 }
 
 export default Products;

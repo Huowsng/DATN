ac93fd9c367d95d0712e033a54b13fb9dc6c831a
Huowsng <huongvlqt007@gmail.com>
2024-05-14 15:38:45 +0700

    14/5

--- 02_WEB/src/App.js
+++ 02_WEB/src/App.js
@@ -1,26 +1,26 @@
-import React, {useState} from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { DataProvider } from './GlobalState';
-import Header from './components/Header/Header';
-import Footer from './components/footer/Footer';
-import MainPages from './components/mainpages/Pages';
-import { TopHeader } from './components/top-header/TopHeader';
+import React, { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { DataProvider } from "./GlobalState";
+import Header from "./components/Header/Header";
+import Footer from "./components/footer/Footer";
+import MainPages from "./components/mainpages/Pages";
+import { TopHeader } from "./components/top-header/TopHeader";
 
 function App() {
-    const [showFooter, setShowFooter] = useState(true);
-    const hideFooter = () => {
-        setShowFooter(false);
-    }
-    return (
-        <BrowserRouter>
-            <DataProvider>
-                <TopHeader />
-                <Header />
-                <MainPages />
-                {showFooter && <Footer />}
-            </DataProvider>
-        </BrowserRouter>
-    );
+  const [showFooter, setShowFooter] = useState(true);
+  const hideFooter = () => {
+    setShowFooter(false);
+  };
+  return (
+    <BrowserRouter>
+      <DataProvider>
+        <TopHeader />
+        <Header />
+        <MainPages />
+        {showFooter && <Footer />}
+      </DataProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- 02_WEB/src/components/mainpages/postnews/postnews.js
+++ 02_WEB/src/components/mainpages/postnews/postnews.js
@@ -1,31 +1,19 @@
-import React, { useContext, useState } from 'react';
-import { GlobalState } from '../../../GlobalState';
-import ProductItem from '../utils/productItem/ProductItem';
-import Loading from '../utils/loading/Loading';
-
-
-import { Link } from 'react-router-dom';
+import { GlobalState } from "../../../GlobalState";
+import React, { useContext, useEffect, useState } from "react";
 
 function Postnews() {
-    const state = useContext(GlobalState);
-    const [products, setProducts] = state.productsAPI.products;
-    console.log(products);
-    const [isAdmin] = state.userAPI.isAdmin;
-    const [token] = state.token;
-    const [callback, setCallback] = state.productsAPI.callback;
-   
-   
-    
+  const state = useContext(GlobalState);
+  const [products, setProducts] = state.productsAPI.products;
+  console.log(products);
+  const [isAdmin] = state.userAPI.isAdmin;
+  const [token] = state.token;
+  const [callback, setCallback] = state.productsAPI.callback;
 
-
-    return (
-        <>
-       
-           
-
-            
-        </>
-    );
+  return (
+    <>
+      <div className="main_postnew"></div>
+    </>
+  );
 }
 
 export default Postnews;
